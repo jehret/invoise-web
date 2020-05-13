30bdb224b6688d18ae5b2c8938f921fc26fd902b
jp <jp.ehret@laposte.net>
2020-05-14 10:05:50 +1200

    Modularité

---
+++ src/main/java/com/mycompany/invoise/InvoiseWebApplication.java
@@ -0,0 +1,15 @@
+package com.mycompany.invoise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
+
+@SpringBootApplication
+public class InvoiseWebApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(InvoiseWebApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/mycompany/invoise/invoiseweb/controller/InvoiceControllerWeb.java
@@ -0,0 +1,43 @@
+package com.mycompany.invoise.invoiseweb.controller;
+
+import com.mycompany.invoise.core.controller.InvoiceControllerInterface;
+import com.mycompany.invoise.core.entity.Invoice;
+
+import com.mycompany.invoise.core.service.InvoiceServiceInterface;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+public class InvoiceControllerWeb implements InvoiceControllerInterface {
+
+    @Autowired
+    private InvoiceServiceInterface invoiceService;
+
+    public InvoiceServiceInterface getInvoiceService() {
+        return invoiceService;
+    }
+
+    public void setInvoiceService(InvoiceServiceInterface invoiceService) {
+        this.invoiceService = invoiceService;
+    }
+
+    public void createInvoice(){
+
+        String customerName="Tesla";
+        Invoice invoice=new Invoice();
+        invoice.setCustomerName(customerName);
+
+        invoiceService.createInvoice(invoice);
+    }
+
+    @RequestMapping("/invoice-home")
+    public @ModelAttribute("invoices") List<Invoice> displayHome(){
+        System.out.println("La méthode display Home a été invoquée");
+        List<Invoice> invoices=invoiceService.getInvoiceList();
+        return invoices;
+    }
+}
