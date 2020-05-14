3119d6a9fd6a14c4489f8ad72bccde5293ba4cc0
jp <jp.ehret@laposte.net>
2020-05-14 17:16:39 +1200

    Path Variable and getById

--- src/main/java/com/mycompany/invoise/invoiseweb/controller/InvoiceControllerWeb.java
+++ src/main/java/com/mycompany/invoise/invoiseweb/controller/InvoiceControllerWeb.java
@@ -6,12 +6,16 @@ import com.mycompany.invoise.core.entity.Invoice;
 import com.mycompany.invoise.core.service.InvoiceServiceInterface;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
 
 @Controller
+@RequestMapping("/invoice")
 public class InvoiceControllerWeb implements InvoiceControllerInterface {
 
     @Autowired
@@ -34,10 +38,22 @@ public class InvoiceControllerWeb implements InvoiceControllerInterface {
         invoiceService.createInvoice(invoice);
     }
 
-    @RequestMapping("/invoice-home")
-    public @ModelAttribute("invoices") List<Invoice> displayHome(){
+    @RequestMapping("/home")
+    public String displayHome(Model model){
         System.out.println("La méthode display Home a été invoquée");
-        List<Invoice> invoices=invoiceService.getInvoiceList();
-        return invoices;
+
+        model.addAttribute("invoices",invoiceService.getInvoiceList());
+        return "invoice-home";
     }
+
+    @RequestMapping("/{id}")
+    public String displayInvoice(@PathVariable("id") String number, Model model){
+        System.out.println("La méthode displayInvoice a été invoquée");
+
+        model.addAttribute("invoice",invoiceService.getInvoiceByNumber(number));
+        //List<Invoice> invoices=invoiceService.getInvoiceList();
+        return "invoice-details";
+    }
+
+
 }
