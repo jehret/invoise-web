85bc1f5c177cceb1eb976be6030d9f6ecbe64115
jp <jp.ehret@laposte.net>
2020-05-28 09:40:55 +1200

    validation de formulaire

--- src/main/java/com/mycompany/invoise/invoiseweb/controller/InvoiceControllerWeb.java
+++ src/main/java/com/mycompany/invoise/invoiseweb/controller/InvoiceControllerWeb.java
@@ -4,17 +4,20 @@ import com.mycompany.invoise.core.controller.InvoiceControllerInterface;
 import com.mycompany.invoise.core.entity.Invoice;
 
 import com.mycompany.invoise.core.service.InvoiceServiceInterface;
+import com.mycompany.invoise.invoiseweb.form.InvoiceForm;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @Controller
 @RequestMapping("/invoice")
-public class InvoiceControllerWeb implements InvoiceControllerInterface {
+public class InvoiceControllerWeb {
 
     @Autowired
     private InvoiceServiceInterface invoiceService;
@@ -28,9 +31,15 @@ public class InvoiceControllerWeb implements InvoiceControllerInterface {
     }
 
     @PostMapping
-    public String createInvoice(@ModelAttribute Invoice invoice){
+    public String createInvoice(@Valid @ModelAttribute InvoiceForm invoiceForm, BindingResult results){
         //vous pourriez même supprimer l'annotation @ModelAttribute si vous ne comptez
         //pas donner un identifiant personnalisé au backing bean
+        if (results.hasErrors()){
+            return "invoice-create-form";
+        }
+        Invoice invoice = new Invoice();
+        invoice.setCustomerName(invoiceForm.getCustomerName());
+        invoice.setOrderNumber(invoiceForm.getOrderNumber());
         invoiceService.createInvoice(invoice);
         return "invoice-created";
     }
@@ -53,7 +62,7 @@ public class InvoiceControllerWeb implements InvoiceControllerInterface {
     }
 
     @GetMapping("/create-form")
-    public String displayInvoiceCreateForm(@ModelAttribute Invoice invoice){
+    public String displayInvoiceCreateForm(@ModelAttribute InvoiceForm invoice){
         //vous pourriez même supprimer l'annotation @ModelAttribute si vous ne comptez
         //pas donner un identifiant personnalisé au backing bean
         return "invoice-create-form";
