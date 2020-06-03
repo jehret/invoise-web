e9440c7aff8e7eef16808ecd8a2c89c5f65908ac
jp <jp.ehret@laposte.net>
2020-06-03 14:19:23 +1200

    front end

--- src/main/java/com/mycompany/invoise/invoiseweb/controller/InvoiceControllerWeb.java
+++ src/main/java/com/mycompany/invoise/invoiseweb/controller/InvoiceControllerWeb.java
@@ -30,7 +30,7 @@ public class InvoiceControllerWeb {
         this.invoiceService = invoiceService;
     }
 
-    /*@PostMapping
+    @PostMapping("/create")
     public String createInvoice(@Valid @ModelAttribute InvoiceForm invoiceForm, BindingResult results){
         //vous pourriez même supprimer l'annotation @ModelAttribute si vous ne comptez
         //pas donner un identifiant personnalisé au backing bean
@@ -42,7 +42,7 @@ public class InvoiceControllerWeb {
         invoice.setOrderNumber(invoiceForm.getOrderNumber());
         invoiceService.createInvoice(invoice);
         return "invoice-created";
-    }*/
+    }
 
     @GetMapping("/home")
     public String displayHome(Model model){
