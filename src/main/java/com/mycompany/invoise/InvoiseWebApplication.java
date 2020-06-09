942f78d17c46941fbc8d1cd318a0a58947c6b3c4
jp <jp.ehret@laposte.net>
2020-06-09 12:35:58 +1200

    connection db

--- src/main/java/com/mycompany/invoise/InvoiseWebApplication.java
+++ src/main/java/com/mycompany/invoise/InvoiseWebApplication.java
@@ -2,14 +2,43 @@ package com.mycompany.invoise;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.PropertySource;
+
+import javax.management.Query;
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 @SpringBootApplication
 public class InvoiseWebApplication {
 
 	public static void main(String[] args) {
 
-		SpringApplication.run(InvoiseWebApplication.class, args);
+		ApplicationContext context=SpringApplication.run(InvoiseWebApplication.class, args);
+		DataSource ds=context.getBean(DataSource.class);
+		Connection conn=null;
+		try {
+			conn=ds.getConnection();
+			System.out.println("success");
+			ResultSet rs=conn.createStatement().executeQuery("SELECT INVOICE_NUMBER,CUSTOMER_NAME FROM INVOICE");
+			while (rs.next()){
+				System.out.println(rs.getLong("INVOICE_NUMBER")+" | "+rs.getString("CUSTOMER_NAME"));
+			}
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		finally {
+			try {
+				if (conn!=null) {
+					conn.close();
+				}
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
 	}
 
 }
