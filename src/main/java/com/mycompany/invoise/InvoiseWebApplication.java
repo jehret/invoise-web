e638494c98ca30b787de7bcf5c5269445990b9b4
jp <jp.ehret@laposte.net>
2020-06-13 20:11:12 +1200

    spring data jpa

--- src/main/java/com/mycompany/invoise/InvoiseWebApplication.java
+++ src/main/java/com/mycompany/invoise/InvoiseWebApplication.java
@@ -2,15 +2,6 @@ package com.mycompany.invoise;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.PropertySource;
-import org.springframework.data.jdbc.repository.config.EnableJdbcRepositories;
-
-import javax.management.Query;
-import javax.sql.DataSource;
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
 
 @SpringBootApplication
 public class InvoiseWebApplication {
