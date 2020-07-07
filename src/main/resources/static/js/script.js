eaf75b6db78042ee0268213e0d27cf2c92dc56be
jp <jp.ehret@laposte.net>
2020-07-07 17:15:35 +1200

    error

--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -58,5 +58,11 @@ function showDetail(invoiceNumber){
         p.appendChild(text);
         invoiceDetailNode.appendChild(p);
 
+        p = document.createElement("p");
+        text = document.createTextNode(`Order number: ${res.orderNumber}`);
+        p.appendChild(text);
+        invoiceDetailNode.appendChild(p);
+
+
     });
 }
