c4784c8e7ac738b2cf225b398f2ea649816aeb9b
Edouard Leroy <leroyedouard@protonmail.com>
2024-07-24 21:16:11 +0200

    Maj application.properties

--- src/main/java/com/mycompany/invoise/invoiseweb/controller/InvoiceControllerWeb.java
+++ src/main/java/com/mycompany/invoise/invoiseweb/controller/InvoiceControllerWeb.java
@@ -7,12 +7,9 @@ import com.mycompany.invoise.core.service.InvoiceServiceInterface;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.ModelAndView;
 
-import java.util.List;
 
 @Controller
 @RequestMapping("/invoice")
@@ -23,6 +20,11 @@ public class InvoiceControllerWeb implements InvoiceControllerInterface {
 
     public InvoiceServiceInterface getInvoiceService() {
         return invoiceService;
+    }
+
+    @Override
+    public String createInvoice(Invoice invoice) {
+        return "";
     }
 
     public void setInvoiceService(InvoiceServiceInterface invoiceService) {
