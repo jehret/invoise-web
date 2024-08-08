5c9cd1da35801325f70b0004d0b4c3783d4e3d6f
Edouard Leroy <leroyedouard@protonmail.com>
2024-08-08 19:17:53 +0200

    format code propre + modifications suite erreurs

--- src/main/java/com/mycompany/invoise/invoiseweb/controller/InvoiceControllerWeb.java
+++ src/main/java/com/mycompany/invoise/invoiseweb/controller/InvoiceControllerWeb.java
@@ -1,19 +1,18 @@
 package com.mycompany.invoise.invoiseweb.controller;
 
+import com.mycompany.invoise.core.controller.InvoiceControllerInterface;
 import com.mycompany.invoise.core.entity.Invoice;
+
 import com.mycompany.invoise.core.service.InvoiceServiceInterface;
-import com.mycompany.invoise.invoiseweb.form.InvoiceForm;
-import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.List;
 
 @Controller
-@RequestMapping("/invoice")
-public class InvoiceControllerWeb {
+public class InvoiceControllerWeb implements InvoiceControllerInterface {
 
     @Autowired
     private InvoiceServiceInterface invoiceService;
@@ -26,34 +25,19 @@ public class InvoiceControllerWeb {
         this.invoiceService = invoiceService;
     }
 
-    @PostMapping
-    public String createInvoice(@Valid @ModelAttribute InvoiceForm invoiceForm, BindingResult results){
-        //vous pourriez même supprimer l'annotation @ModelAttribute si vous ne comptez
-        //pas donner un identifiant personnalisé au backing bean
-        if (results.hasErrors()){
-            return "invoice-create-form";
-        }
-        Invoice invoice = new Invoice();
-        invoice.setCustomerName(invoiceForm.getCustomerName());
-        invoice.setOrderNumber(invoiceForm.getOrderNumber());
+    public void createInvoice(){
+
+        String customerName="Tesla";
+        Invoice invoice=new Invoice();
+        invoice.setCustomerName(customerName);
+
         invoiceService.createInvoice(invoice);
-        return "invoice-created";
     }
 
-    @GetMapping("/home")
-    public String displayHome(Model model){
+    @RequestMapping("/invoice-home")
+    public @ModelAttribute("invoices") List<Invoice> displayHome(){
         System.out.println("La méthode display Home a été invoquée");
-
-        model.addAttribute("invoices",invoiceService.getInvoiceList());
-        return "invoice-home";
-    }
-
-    @GetMapping("/{id}")
-    public String displayInvoice(@PathVariable("id") String number, Model model){
-        System.out.println("La méthode displayInvoice a été invoquée");
-
-        model.addAttribute("invoice",invoiceService.getInvoiceByNumber(number));
-        //List<Invoice> invoices=invoiceService.getInvoiceList();
-        return "invoice-details";
+        List<Invoice> invoices=invoiceService.getInvoiceList();
+        return invoices;
     }
 }
