ee1e9fa6aa88d10b6bd40860be0d92c0c9948cda
Edouard Leroy <leroyedouard@protonmail.com>
2024-08-09 08:11:15 +0200

    format code propre + ajout de dependance jakarta-validation

--- src/main/java/com/mycompany/invoise/invoiseweb/controller/InvoiceControllerWeb.java
+++ src/main/java/com/mycompany/invoise/invoiseweb/controller/InvoiceControllerWeb.java
@@ -1,8 +1,6 @@
 package com.mycompany.invoise.invoiseweb.controller;
 
-import com.mycompany.invoise.core.controller.InvoiceControllerInterface;
 import com.mycompany.invoise.core.entity.Invoice;
-
 import com.mycompany.invoise.core.service.InvoiceServiceInterface;
 import com.mycompany.invoise.invoiseweb.form.InvoiceForm;
 import jakarta.validation.Valid;
@@ -11,7 +9,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.ModelAndView;
+
 
 @Controller
 @RequestMapping("/invoice")
@@ -29,10 +27,10 @@ public class InvoiceControllerWeb {
     }
 
     @PostMapping
-    public String createInvoice(@Valid @ModelAttribute InvoiceForm invoiceForm, BindingResult results){
+    public String createInvoice(@Valid @ModelAttribute InvoiceForm invoiceForm, BindingResult results) {
         //vous pourriez même supprimer l'annotation @ModelAttribute si vous ne comptez
         //pas donner un identifiant personnalisé au backing bean
-        if (results.hasErrors()){
+        if (results.hasErrors()) {
             return "invoice-create-form";
         }
         Invoice invoice = new Invoice();
@@ -43,24 +41,24 @@ public class InvoiceControllerWeb {
     }
 
     @GetMapping("/home")
-    public String displayHome(Model model){
+    public String displayHome(Model model) {
         System.out.println("La méthode display Home a été invoquée");
 
-        model.addAttribute("invoices",invoiceService.getInvoiceList());
+        model.addAttribute("invoices", invoiceService.getInvoiceList());
         return "invoice-home";
     }
 
     @GetMapping("/{id}")
-    public String displayInvoice(@PathVariable("id") String number, Model model){
+    public String displayInvoice(@PathVariable("id") String number, Model model) {
         System.out.println("La méthode displayInvoice a été invoquée");
 
-        model.addAttribute("invoice",invoiceService.getInvoiceByNumber(number));
+        model.addAttribute("invoice", invoiceService.getInvoiceByNumber(number));
         //List<Invoice> invoices=invoiceService.getInvoiceList();
         return "invoice-details";
     }
 
     @GetMapping("/create-form")
-    public String displayInvoiceCreateForm(@ModelAttribute InvoiceForm invoice){
+    public String displayInvoiceCreateForm(@ModelAttribute InvoiceForm invoice) {
         //vous pourriez même supprimer l'annotation @ModelAttribute si vous ne comptez
         //pas donner un identifiant personnalisé au backing bean
         return "invoice-create-form";
