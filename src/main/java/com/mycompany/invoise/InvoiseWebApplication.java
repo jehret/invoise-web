16f4944ed574b25b63cac87868037c655627a0a2
Edouard Leroy <leroyedouard@protonmail.com>
2024-08-21 18:50:33 +0200

    format code propre (CONTROLE)

--- src/main/java/com/mycompany/invoise/InvoiseWebApplication.java
+++ src/main/java/com/mycompany/invoise/InvoiseWebApplication.java
@@ -2,22 +2,14 @@ package com.mycompany.invoise;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.PropertySource;
-
-import javax.management.Query;
-import javax.sql.DataSource;
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
 
 @SpringBootApplication
 public class InvoiseWebApplication {
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 
-		SpringApplication.run(InvoiseWebApplication.class, args);
+        SpringApplication.run(InvoiseWebApplication.class, args);
 
-	}
+    }
 
 }
