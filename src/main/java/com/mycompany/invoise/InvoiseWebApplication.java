11587e2e223d5636ed3a9a28e3c97ad0aa218690
Edouard Leroy <leroyedouard@protonmail.com>
2024-08-21 18:30:20 +0200

    format code propre (CONTROLE)

--- src/main/java/com/mycompany/invoise/InvoiseWebApplication.java
+++ src/main/java/com/mycompany/invoise/InvoiseWebApplication.java
@@ -3,9 +3,7 @@ package com.mycompany.invoise;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.PropertySource;
 
-import javax.management.Query;
 import javax.sql.DataSource;
 import java.sql.Connection;
 import java.sql.ResultSet;
@@ -14,31 +12,30 @@ import java.sql.SQLException;
 @SpringBootApplication
 public class InvoiseWebApplication {
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 
-		ApplicationContext context=SpringApplication.run(InvoiseWebApplication.class, args);
-		DataSource ds=context.getBean(DataSource.class);
-		Connection conn=null;
-		try {
-			conn=ds.getConnection();
-			System.out.println("success");
-			ResultSet rs=conn.createStatement().executeQuery("SELECT INVOICE_NUMBER,CUSTOMER_NAME FROM INVOICE");
-			while (rs.next()){
-				System.out.println(rs.getLong("INVOICE_NUMBER")+" | "+rs.getString("CUSTOMER_NAME"));
-			}
+        ApplicationContext context = SpringApplication.run(InvoiseWebApplication.class, args);
+        DataSource ds = context.getBean(DataSource.class);
+        Connection conn = null;
+        try {
+            conn = ds.getConnection();
+            System.out.println("success");
+            ResultSet rs = conn.createStatement().executeQuery("SELECT INVOICE_NUMBER,CUSTOMER_NAME FROM INVOICE");
+            while (rs.next()) {
+                System.out.println(rs.getLong("INVOICE_NUMBER") + " | " + rs.getString("CUSTOMER_NAME"));
+            }
 
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-		finally {
-			try {
-				if (conn!=null) {
-					conn.close();
-				}
-			} catch (SQLException e) {
-				e.printStackTrace();
-			}
-		}
-	}
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (conn != null) {
+                    conn.close();
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 
 }
