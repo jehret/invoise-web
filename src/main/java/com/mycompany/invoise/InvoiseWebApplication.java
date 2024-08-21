5e7c9d177a516088cc57e12b9b4c5e2175f7cbe0
Edouard Leroy <leroyedouard@protonmail.com>
2024-08-21 19:59:08 +0200

    format code propre (CONTROLE) + modif dependance starter

--- src/main/java/com/mycompany/invoise/InvoiseWebApplication.java
+++ src/main/java/com/mycompany/invoise/InvoiseWebApplication.java
@@ -6,10 +6,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class InvoiseWebApplication {
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 
-		SpringApplication.run(InvoiseWebApplication.class, args);
+        SpringApplication.run(InvoiseWebApplication.class, args);
 
-	}
+    }
 
 }
